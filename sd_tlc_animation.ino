f7eed77ef120d453087cab0a7ddfeb84431cd732
Alejandro J. Cura <alecu@protocultura.net>
2016-04-05 21:48:27 -0300

    Leer cuadro a cuadro, y pasarlo al TLC

--- sd_tlc_animation.ino
+++ sd_tlc_animation.ino
@@ -3,11 +3,15 @@
 #include <SPI.h>
 
 const int chipSelect = 10;
+const int HALL_SENSOR = 2;
 
+File animFolder;
 File dataFile;
+#define DEBUG(x) Serial.println(x)
 
 void PrintHex8(uint8_t *data, uint8_t length)
 {
+#if DEBUG
   Serial.print("0x"); 
   for (int i=0; i<length; i++) { 
     if (data[i]<0x10) {
@@ -16,39 +20,61 @@ void PrintHex8(uint8_t *data, uint8_t length)
     Serial.print(data[i],HEX); 
     Serial.print(" "); 
   }
+#endif
+}
+
+
+volatile unsigned long last_turn = 0;
+volatile unsigned long last_turn_duration = 10L;
+volatile bool new_loop = true;
+
+void hall_activated()
+{
+  unsigned long this_turn = micros();
+  unsigned long this_turn_duration = this_turn - last_turn;
+  last_turn_duration = this_turn_duration;
+  last_turn = this_turn;
+  new_loop = true;;
 }
 
 void setup()
 {
   Tlc.init();
+
+#if DEBUG
   Serial.begin(115200);
   while (!Serial) {
   }
+#endif
 
-  Serial.print("Initializing SD card... a, b, c...");
-  
+  pinMode(HALL_SENSOR, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(HALL_SENSOR), hall_activated, FALLING);
+
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
+    DEBUG("Card failed, or not present");
     return;
   }
-  Serial.println("card initialized.");
-  dataFile = SD.open("degrade.bin");
-  if (!dataFile) {
-    Serial.println("error opening degrade.bin");
+  DEBUG("card initialized.");
+
+  animFolder = SD.open("anim");
+  if (!animFolder) {
+    DEBUG("no hay carpeta de animaciones");
   }
 }
 
 void loop()
-{
-  //Serial.print(dataFile.position());
-  //Serial.print(") ");
-  dataFile.read(tlc_GSData, NUM_TLCS * 24);
-  //PrintHex8(tlc_GSData, 12);
-  //Serial.println();
-  Tlc.update();
-
-  if (dataFile.available() == false) {
-    dataFile.seek(0);
+{  
+  if (new_loop) {
+    new_loop = false;
+    dataFile = animFolder.openNextFile();
+    if (!dataFile) {
+      animFolder.rewindDirectory();
+      dataFile = animFolder.openNextFile();
+    }
   }
-
+  if (dataFile.available()) {
+    dataFile.read(tlc_GSData, NUM_TLCS * 24);
+    Tlc.update();
+    delayMicroseconds(200);
+  }
 }
